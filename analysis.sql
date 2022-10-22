f8c0f10151514cf24f48eb2efd90808245a2b5ca
twigikit <twigikit@hotmail.com>
2022-10-22 16:16:26 +1100

    sql analysis

--- analysis.sql
+++ analysis.sql
@@ -1,20 +1,19 @@
 
 --count the transactions that are less than $2.00 per cardholder
 CREATE VIEW  small_transaction AS
-SELECT  f.name,
-		count(name) as transaction_count
+SELECT  cardholder_id,
+		count(cardholder_id) as small_trans_cnt
 FROM 	(	--select all transaction amount less than $2.00
 		 	SELECT 	t1.id,
 					t1.date,
 					t1.amount,
-					t1.card,
-					t3.name	
-			FROM 	transaction t1, credit_card t2, card_holder t3
+					t2.cardholder_id
+			FROM 	transaction t1, credit_card t2
 			WHERE 	t1.card = t2.card
-			AND 	t2.cardholder_id = t3.id
 			AND 	amount < 2
 	 	 )f
-GROUP BY f.name
-ORDER BY transaction_count DESC
+GROUP BY cardholder_id
+ORDER BY small_trans_cnt DESC
 ;
+
 
