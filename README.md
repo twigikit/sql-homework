c0265d3b9cd881bda2c57a49874e18b5e9ed03c9
twigikit <twigikit@hotmail.com>
2022-10-22 23:35:03 +1100

    Update ReadMe

--- README.md
+++ README.md
@@ -8,11 +8,25 @@ The Data Analytics team has reviewed 25 credit card holders historical transacti
 
 CFO has also requested a more detailed analysis on specific card holders. This README presents the observations and conclusion of the review and the analysis. 
 
-### Data and Methodology
+### Data and Model
 To study the consumption pattern, we have relied on daily credit card transaction data for the period from 01 Jan 2018 to 31 December 2018. A database model was defined to store the credit card transaction data and a PostgreSQL database was created based on the model. The entity relationship diagram below sets out the defined database model.
 
 ![](./Diagram/ERD_Fraud_Detection.jpg)
 
 ### Analysis Results
-#### Potential Fraudelent Transactions
+#### Part 1. Potential Fraudelent Transactions
 
+
+#### Part 2. Specific Card Holders
+**Card holder 2**
+
+![](./Diagram/Plot_Cardholder_2.png)
+
+**Card holder 18**
+![](./Diagram/Plot_Cardholder_18.png)
+
+
+**Card holder 25**
+
+
+![](./Diagram/Boxplot_Cardholder_25.png)
--- analysis.sql
+++ analysis.sql
@@ -1,19 +1,65 @@
+/* Data preparation
+Isolate (or group ) the transaction by each cardholder
+ - join transaction and credit card tables where the card number is matching.
+ - flag transaction that is less than $2.00 */
+ 
+ CREATE VIEW v_data_joined AS
+ SELECT t1.id AS txn_id,
+		t1.date,
+		EXTRACT(DAY FROM date) AS txn_day,
+		EXTRACT(MONTH FROM date) AS txn_month,
+		EXTRACT(HOUR FROM date) AS txn_hour,
+		t2.cardholder_id,
+		t1.amount,
+		t3.name AS merchant_name,
+		t4.name AS merchant_category,
+		CASE 
+		 	WHEN (amount < 2)  THEN 1 
+			WHEN (amount >= 2) 	THEN 0
+		 END AS twoless_ind
+ FROM 	transaction t1, 
+ 		credit_card t2, 
+		merchant t3, 
+		merchant_category t4
+ WHERE 	t1.card = t2.card
+ AND 	t1.id_merchant = t3.id
+ AND 	t3.id_merchant_category = t4.id
+ ;
+
+/* review the transaction by cardholder */
+SELECT 		
+			cardholder_id,
+			EXTRACT(MONTH FROM date) AS txn_month,
+			EXTRACT(DAY FROM date) AS txn_day,
+			min(amount) AS min_amount,
+			ROUND(CAST(max(amount) AS numeric),2) AS max_amount,
+			ROUND(CAST(sum(amount) AS numeric),2) AS total_amount,
+			ROUND(CAST(avg(amount) AS numeric),2) AS average_amount,
+			sum(twoless_ind) AS num_txn_twoless,
+			count(cardholder_id) AS num_txn_all
+FROM 		data_all
+GROUP BY	cardholder_id, txn_month, txn_day
+ORDER BY 	cardholder_id, txn_month, txn_day
+;
 
 --count the transactions that are less than $2.00 per cardholder
-CREATE VIEW  small_transaction AS
-SELECT  cardholder_id,
-		count(cardholder_id) as small_trans_cnt
-FROM 	(	--select all transaction amount less than $2.00
-		 	SELECT 	t1.id,
-					t1.date,
-					t1.amount,
-					t2.cardholder_id
-			FROM 	transaction t1, credit_card t2
-			WHERE 	t1.card = t2.card
-			AND 	amount < 2
-	 	 )f
-GROUP BY cardholder_id
-ORDER BY small_trans_cnt DESC
+CREATE VIEW v_txn_twoless AS 
+SELECT 
+			cardholder_id,
+			sum(twoless_ind) AS num_txn_twoless
+FROM		v_data_joined
+GROUP BY	cardholder_id
+ORDER BY 	cardholder_id
 ;
+
+--top 100 highest transactions made between 7am and 9am
+CREATE VIEW v_top_100_txn_am AS
+SELECT		*
+FROM		V_data_joined
+WHERE		txn_hour IN (7,8,9)
+ORDER BY 	amount DESC
+LIMIT 100
+
+
 
 
