4f826b6c17fb1383868538df44e2c15438af9f19
twigikit <twigikit@hotmail.com>
2022-10-22 14:52:30 +1100

    Upload Entity Relationship Diagram

--- create_table.sql
+++ create_table.sql
@@ -9,7 +9,7 @@
 CREATE TABLE
 	card_holder
 	( 
-	  id integer PRIMARY KEY,
+	  id integer PRIMARY KEY NOT NULL,
 	  name varchar(50)
 	);
 
@@ -17,35 +17,33 @@ CREATE TABLE
 	credit_card
 	( 
 	  card varchar(20),
-	  cardholder_id integer,
+	  cardholder_id integer NOT NULL,
  	  FOREIGN KEY (cardholder_id) REFERENCES card_holder(id)
 	);
 	
 CREATE TABLE
 	merchant_category
 	(
-		id integer PRIMARY KEY,
+		id integer PRIMARY KEY NOT NULL,
 		name varchar(50)
 	);
 	
 CREATE TABLE
 	merchant
 	(
-		id integer PRIMARY KEY,
+		id integer PRIMARY KEY NOT NULL,
 		name varchar(50),
-		id_merchant_category integer,
+		id_merchant_category integer NOT NULL,
 		FOREIGN KEY (id_merchant_category) REFERENCES merchant_category(id)
 	);
 
 CREATE TABLE
 	transaction
 	(
-		id integer PRIMARY KEY,
+		id integer PRIMARY KEY NOT NULL,
 		date timestamp,
 		amount float,
-		card varchar(20),
-		id_merchant integer,
+		card varchar(20) NOT NULL,
+		id_merchant integer NOT NULL,
 		FOREIGN KEY (id_merchant) REFERENCES merchant(id)
 	);
-
-
